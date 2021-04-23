0fdfe12cc48c0ddea241be7ae0fb5c7e095d541f
jorge <roquijo@hotmail.com>
2021-04-23 08:22:25 -0500

    mj

--- AplicacionTestCadenas/Grafica.cs
+++ AplicacionTestCadenas/Grafica.cs
@@ -27,7 +27,7 @@ namespace AplicacionTestCadenas
         public const int DIAMETRO = 30;
         public const int ANCHO = 50;
         public const int RADIO = DIAMETRO / 2;
-        public const int MEDIDARAIZ = DIAMETRO + RADIO;
+        public const int MEDIDAELIPSE = DIAMETRO + RADIO;
 
         private void mostrarArbol(Graphics pGrafica, int x, int y, Nodo pNodo)
         {
@@ -35,64 +35,62 @@ namespace AplicacionTestCadenas
             Pen pElipse = new Pen(Color.Red);
             Brush b = new SolidBrush(this.ForeColor);
 
-
             if (pNodo == null)
             {
                
             }
             else
-            {
-                int anchoSeparacion = Nodo.darTodoNodos(pNodo) * (ANCHO / 2);
-
+            {            
+                int anchoSeparacion = x / (ServicioArbolBinario.darNodos().Length-1);
                 String cadena = pNodo.getDatos();
                 switch (cadena)
                 {
 
                     case "*":
                         
-                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDARAIZ, MEDIDARAIZ);
+                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDAELIPSE, MEDIDAELIPSE);
                         pGrafica.DrawString(pNodo.getDatos(), new Font("Verdana", 15), b, x + 3, y - 6);
                         break;
 
                     case "+":
 
-                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDARAIZ, MEDIDARAIZ);
+                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDAELIPSE, MEDIDAELIPSE);
                         pGrafica.DrawString(pNodo.getDatos(), new Font("Verdana", 15), b, x + 2, y - 13);
                         break;
 
                     case "&":
 
-                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDARAIZ, MEDIDARAIZ);
+                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDAELIPSE, MEDIDAELIPSE);
                         pGrafica.DrawString(pNodo.getDatos(), new Font("Verdana", 15), b, x + 3, y - 10);
                         break;
 
                     case "%":
 
-                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDARAIZ, MEDIDARAIZ);
+                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDAELIPSE, MEDIDAELIPSE);
                         pGrafica.DrawString(pNodo.getDatos(), new Font("Verdana", 15), b, x , y - 10);
                         break;
 
                     case "#":
 
-                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDARAIZ, MEDIDARAIZ);
+                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDAELIPSE, MEDIDAELIPSE);
                         pGrafica.DrawString(pNodo.getDatos(), new Font("Verdana", 15), b, x + 2, y - 10);
                         break;
 
                     case "/":
 
-                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDARAIZ, MEDIDARAIZ);
+                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDAELIPSE, MEDIDAELIPSE);
                         pGrafica.DrawString(pNodo.getDatos(), new Font("Verdana", 15), b, x + 5, y - 12);
                         break;
 
                     case "$":
 
-                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDARAIZ, MEDIDARAIZ);
+                        pGrafica.DrawEllipse(pElipse, x - 10, y - 20, MEDIDAELIPSE, MEDIDAELIPSE);
                         pGrafica.DrawString(pNodo.getDatos(), new Font("Verdana", 15), b, x + 3, y - 12);
                         break;
 
                     default:
 
-                        pGrafica.DrawEllipse(pDatos, x - 10, y - 20, MEDIDARAIZ, MEDIDARAIZ);
+                        pGrafica.DrawEllipse(pDatos, x - 10, y - 20, MEDIDAELIPSE, MEDIDAELIPSE);
                         pGrafica.DrawString(pNodo.getDatos(), new Font("Verdana", 15), b, x + 3, y - 12);
                         break;
                 }
